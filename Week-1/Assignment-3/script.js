80cd46cc738862937e86a3cd1a7ebc71f6519d48
tzutingspace <tzutingh2@gmail.com>
2023-01-07 16:30:42 +0800

    Add Week-1_assignment-3

---
+++ Week-1/Assignment-3/script.js
@@ -0,0 +1,36 @@
+function countAandB(input) {
+    // your code here
+    var count_a = 0;
+    var count_b = 0;
+    for (var char of input) {
+        if (char == 'a') {
+            count_a += 1
+        }
+        else if (char == 'b') {
+            count_b += 1
+        }
+    }
+    if ((count_a + count_b) != 0) {
+        output = `${count_a + count_b} (${count_a} 'a' letter and ${count_b} 'b' letter)`;
+    }
+    else {
+        output = `${count_a + count_b}`;
+    }
+    return output
+}
+function toNumber(input) {
+    // your code here
+    var output = [];
+    for (let char of input) {
+        output.push(char.charCodeAt(0) - 96)
+    }
+    return output
+}
+
+let input1 = ['a', 'b', 'c', 'a', 'c', 'a', 'c'];
+console.log(countAandB(input1)); // should print 4 (3 ‘a’ letters and 1 ‘b’ letter)
+console.log(toNumber(input1)); // should print [1, 2, 3, 1, 3, 1, 3]
+
+let input2 = ['e', 'd', 'c', 'd', 'e'];
+console.log(countAandB(input2)); // should print 0
+console.log(toNumber(input2)); // should print [5, 4, 3, 4, 5]
